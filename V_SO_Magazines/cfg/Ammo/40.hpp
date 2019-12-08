c9d80c0bbde8e1e4f4fd37d66d872e376e29a4d5
Andrew Vespade <zerovespula@gmail.com>
2019-12-07 23:31:01 -0800

    SpecOrd Flares
    Added blue fares for underbarrel grenade launcher

---
+++ V_SO_Magazines/cfg/Ammo/40.hpp
@@ -0,0 +1,11 @@
+class F_40mm_White;
+class F_40mm_Blue: F_40mm_White
+{
+	dlc							= "SO";
+	author						= "Fireteam Zulu";
+	scope						= 2;
+	scopeArsenal				= 2;
+	
+	lightColor[] 				= {0.25,0.25,0.5,0};
+	model 						= "v_fz_weapons\data\flares\flare_blue.p3d";
+};
---
+++ V_SO_Magazines/cfg/Magazines/40.hpp
@@ -0,0 +1,26 @@
+class 3Rnd_UGL_FlareWhite_F;
+class 3Rnd_UGL_FlareBlue_F: 3Rnd_UGL_FlareWhite_F
+{
+	dlc							= "SO";
+	author						= "Fireteam Zulu";
+	scope						= 2;
+	scopeArsenal				= 2;
+	
+	ammo 						= "F_40mm_Blue";
+	displayName 				= "3Rnd 3GL Flares (Blue)";
+	descriptionShort 			= "Type: Flare Rounds - Blue<br />Rounds: 3<br />Used in: 3GL";
+	picture 					= "\v_so_weapons\data\flares\gear_ugl_flare_blue_ca.paa";
+};
+class UGL_FlareWhite_F;
+class UGL_FlareBlue_F: UGL_FlareWhite_F
+{
+	dlc							= "SO";
+	author						= "Fireteam Zulu";
+	scope						= 2;
+	scopeArsenal				= 2;
+	
+	ammo 						= "F_40mm_Blue";
+	displayName 				= "Flare Round (Blue)";
+	descriptionShort 			= "Type: Flare Round - Blue<br />Rounds: 1<br />Used in: ELGM, 3GL";
+	picture 					= "\v_fz_weapons\data\flares\gear_ugl_flare_blue_ca.paa";
+};
