828f2e69d7f9d54828ffc9a87865b996a93bacde
darknightx33x <44118889+darknightx33x@users.noreply.github.com>
2023-05-29 12:24:24 -0400

    Delete V_SO_Weapons directory

--- V_SO_Weapons/cfg/SRS99/SRS99CX.hpp
+++
@@ -1,9 +0,0 @@
-class OPTRE_SRS99C: OPTRE_SRS99D
-{
-	dlc							= "SO";
-	author						= "Fireteam Zulu";
-	scope						= 2;
-	scopeArsenal				= 2;
-	
-	displayName					= "SRS99CX-S2 AM Sniper Rifle";
-};
